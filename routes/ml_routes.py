9c6eeb3983f9c23bc72a8fd11a869c079351626f
Gustavo Loyola <guh.loyola@gmail.com>
2025-06-13 20:09:07 -0300

    Adicionado rotas para chamadas referentes a Machine Learning

---
+++ routes/ml_routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from models.feedback_schema import *
+
+router = APIRouter()
+
+@router.post("/feedback", 
+             response_model=FeedbackResponse, 
+             summary="Enviar feedback sobre uma previsão", 
+             description="Permite que o usuário envie um feedback sobre a previsão do modelo."
+)
+def feedback(feedback: FeedbackRequest):
+   print(f"Feedback recebido para {feedback.prediction_id}: {feedback.correct_label}")
+   return FeedbackResponse(message="Feedback recebido com sucesso!")
