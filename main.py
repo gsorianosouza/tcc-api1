d458268dea58c997d2d54015c8bf14ea08308a90
Gustavo Loyola <guh.loyola@gmail.com>
2025-06-23 20:31:07 -0300

    Adicionada configurações de CORS no main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from routes import system_routes, ml_routes
+from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
 
-app = FastAPI();
+app = FastAPI(title=settings.PROJECT_NAME, version=settings.API_VERSION);
 
 @app.get("/",tags=["Home"], summary="Rota inicial da API")
 def read_root():
     return "Seja Bem-vindo à API!"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origin.strip() for origin in settings.ALLOW_ORIGINS] if isinstance(settings.ALLOW_ORIGINS, str) else settings.ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(system_routes.router, tags= ["Sistema"])
 app.include_router(ml_routes.router, prefix="/model", tags=["Machine Learning"])
