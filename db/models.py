337325d060ed8f7a0a48a610850ebaa97030b1cb
Gustavo Loyola <guh.loyola@gmail.com>
2025-06-30 22:48:47 -0300

    Modelos das tabelas do banco de dados criados

---
+++ db/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+
+from db.database import Base
+
+class Model(Base):
+    __tablename__ = 'model'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    version = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    
+    prediction_id = relationship("Prediction", back_populates="model")
+    
+class Prediction(Base):
+    __tablename__ = 'prediction'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    input_text = Column(String, nullable=False)
+    result = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    
+    model_id = Column(Integer, ForeignKey('model.id'))
+    model = relationship("Model", back_populates="prediction_id")
