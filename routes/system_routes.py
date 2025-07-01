4cce8b4ccd9b42558cd2a04c15c911a4ae8e058e
Gustavo Loyola <guh.loyola@gmail.com>
2025-07-01 19:56:04 -0300

    Rota de post para adicionar um modelo ao banco de dados criada

--- routes/system_routes.py
+++ routes/system_routes.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from schemas.model_schema import ModelSchema
+from sqlalchemy.orm import Session
+from db.deps import get_db
+from db.models import Model
 
 router = APIRouter()
 
@@ -8,4 +12,34 @@ def get_status():
         "status": "ok",
         "model_loaded": True,
         "model_version": "TF-IDF 1.0"
+    }
+    
+@router.post("/add-model", summary="Adiciona um novo modelo")
+def add_model(model: ModelSchema, db: Session = Depends(get_db)):
+
+    if not model.name or not model.version:
+        return { "Message": "Nome e versão do modelo são obrigatórios!"}
+    
+    existing_model = db.query(Model).filter(
+        Model.name == model.name,
+        Model.version == model.version
+    ).first()
+    
+    if existing_model:
+        return { "Message": "Não foi possível adicionar, o modelo já existe no banco de dados" }
+    
+    new_model = Model( name=model.name,version=model.version )
+
+    db.add(new_model)
+    db.commit()
+    db.refresh(new_model)
+
+    return {
+        "Message": "Modelo adicionado com sucesso!",
+        "Model": {
+            "id": new_model.id,
+            "name": new_model.name,
+            "version": new_model.version,
+            "created_at": new_model.created_at.isoformat()
+        }
     }
