ade8168fb2d65e6706aa53fd73d5878c9853b269
Gustavo Loyola <guh.loyola@gmail.com>
2025-06-30 22:48:28 -0300

    Arquivo de incialização das tabelas do banco de dados criado

---
+++ db/init_db.py
@@ -0,0 +1,13 @@
+from db.database import engine
+from db import models
+
+def init_db():
+    # Função para inicializar o nosso banco de dados. Ela cria todas as tabelas definidas em models.py, caso ainda não existam.
+    print("Inicializando o banco de dados...")
+    
+    models.Base.metadata.create_all(bind=engine)
+    
+    print("Tabelas criadas com sucesso!")
+
+if __name__ == "__main__":
+    init_db()
